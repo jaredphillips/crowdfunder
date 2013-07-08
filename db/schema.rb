69bbe0cea985a9c2a4903a2acbcea607ecc6e900
Jared Phillips <jaredtphillips@me.com>
2013-07-08 14:11:32 -0400

    generating Project model with validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130708175547) do
+ActiveRecord::Schema.define(version: 20130708181030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: true do |t|
+    t.string   "title"
+    t.string   "teaser"
+    t.integer  "user_id"
+    t.text     "description"
+    t.integer  "goal"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "projects", ["user_id"], name: "index_projects_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "first_name"
