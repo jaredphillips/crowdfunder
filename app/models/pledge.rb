aed1d1e5f947ebc701056c0e09ebe730ff377414
Alex Naser <c.a.naser@gmail.com>
2013-07-09 13:58:33 -0400

    Pledge unit tests passing

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,4 +1,19 @@
 class Pledge < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+
+  validates :user, :project, presence: true
+  validates :amount,
+  	presence: true,
+  	numericality: true
+
+  validate :validate_amount_greater_than_zero
+
+  protected
+
+  		def validate_amount_greater_than_zero
+				if amount && amount < 0
+					errors.add :amount, "must be greater than 0"
+				end
+			end
 end
